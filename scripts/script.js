0e36aedab9d97c287a575467ed63dbe9fdd80923
Michel Iseneld <84731866+iseneld@users.noreply.github.com>
2021-09-19 20:50:43 +0200

    git stat

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,40 @@
-function createThumbnail(post) {
-  let thumbnail = `
-  <a href="../pages/blog-post.html?id=${post.id}"><article>
-    <img src="${post.previewImage}" alt="image" />
-    <h2>${post.title}</h2>
-    <p>${post.shortSummary}</p>
-  </article></a>
-  `;
-  document.querySelector(".blog__thumbnails").innerHTML += thumbnail;
-}
+// THUMBNAILS
 
 function getThumbnails() {
   fetch("../data/posts.json")
     .then((response) => response.json())
     .then((data) => {
       for (let i = 0; i < data.length; i++) {
-        createThumbnail(data[i]);
+        createThumbnail(data[i]); // Funcion call >>
+      }
+    });
+}
+
+function createThumbnail(post) {
+  let thumbnail = `
+ <article> 
+    <a href="../pages/blog-post.html?id=${post.id}">
+      <img src="${post.previewImage}" alt="image" />
+      <h2>${post.title}</h2>
+      <p>${post.shortSummary}</p>
+    </a>
+  </article>
+  `;
+  document.querySelector(".blog__thumbnails").innerHTML += thumbnail;
+}
+
+// BLOG POST
+
+function getPostFromId() {
+  var id = JSON.parse(findQuery("id")); // Funcion call >>
+
+  fetch("../data/posts.json")
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].id === id) {
+          createPost(data[i]); // Funcion call >>
+        }
       }
     });
 }
@@ -34,31 +54,9 @@ function createPost(post) {
   document.querySelector(".blog__post article").innerHTML += backLink;
 }
 
-function getPosts() {
-  fetch("../data/posts.json")
-    .then((response) => response.json())
-    .then((data) => {
-      for (let i = 0; i < data.length; i++) {
-        createPost(data[i]);
-      }
-    });
-}
+// QUERY URL STRING
 
 function findQuery(param) {
   var urlParams = new URLSearchParams(window.location.search, param);
   return urlParams.get(param);
-}
-
-function getPostFromId() {
-  var id = JSON.parse(findQuery("id"));
-
-  fetch("../data/posts.json")
-    .then((response) => response.json())
-    .then((data) => {
-      for (let i = 0; i < data.length; i++) {
-        if (data[i].id === id) {
-          createPost(data[i]);
-        }
-      }
-    });
 }
